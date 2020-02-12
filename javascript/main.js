ec3249765721c66c1ff0a31aca667801792c41a5
Abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-02-12 07:55:46 +0100

    Setup navbar mobile

---
+++ javascript/main.js
@@ -0,0 +1,23 @@
+const sideNav = document.querySelector('.side-nav');
+const cover = document.querySelector('.cover');
+
+const toggle = document.querySelector('.toggle');
+const closeNav = document.querySelector('.close-nav')
+
+toggle.addEventListener('click', () => {
+  sideNav.style.width = '200px';
+  sideNav.style.display = 'flex';
+  cover.style.display = 'block';
+  closeNav.style.display = 'block'
+})
+
+const hideNav = () => {
+  sideNav.style.width = 0;
+  sideNav.style.display = 'none';
+  cover.style.display = 'none';
+  closeNav.style.display = 'none'
+}
+
+cover.addEventListener('click', hideNav)
+
+closeNav.addEventListener('click', hideNav)
